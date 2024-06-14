4adcc816e19becac6e145163c154143a8b8bdb36
samsonajulor <samsonajulor@gmail.com>
2024-06-14 15:40:07 +0100

    added router and toast

--- src/web3-ic-blog-frontend/src/App.jsx
+++ src/web3-ic-blog-frontend/src/App.jsx
@@ -1,30 +1,37 @@
-import { useState } from 'react';
-import { web3_ic_blog_backend } from 'declarations/web3-ic-blog-backend';
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home';
+import Header from './components/Header';
+import SinglePost from './pages/SinglePost';
+import UserProfile from './pages/UserProfile';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/post/:id',
+    element: <SinglePost />,
+  },
+  {
+    path: '/user',
+    element: <UserProfile />,
+  },
+]);
 
 function App() {
-  const [greeting, setGreeting] = useState('');
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    const name = event.target.elements.name.value;
-    web3_ic_blog_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
-    });
-    return false;
-  }
-
   return (
-    <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
-      </form>
-      <section id="greeting">{greeting}</section>
-    </main>
+    <>
+      <Header />
+      <main className='mt-10'>
+        <RouterProvider router={router} />
+      </main>
+      <ToastContainer />
+    </>
   );
 }
 
