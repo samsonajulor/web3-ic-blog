d5a30c16d6485a3081ccf9bba32acafbd7dee679
samsonajulor <samsonajulor@gmail.com>
2024-06-14 15:37:50 +0100

    created global context

---
+++ src/web3-ic-blog-frontend/src/context/blog.js
@@ -0,0 +1,22 @@
+import { createContext, useContext, useState } from 'react';
+
+const Blog = createContext();
+
+const BlogProvider = ({ children }) => {
+  const [posts, setPosts] = useState();
+
+  return (
+    <Blog.Provider
+      value={{
+        posts,
+        setPosts
+      }}
+    >
+      {children}
+    </Blog.Provider>
+  );
+};
+
+export const useBlog = () => useContext(Blog);
+
+export default BlogProvider;
